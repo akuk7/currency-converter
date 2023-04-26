1b6025b98c878b519c7468bbe3283d2e72f2f612
akuk <amjadkhamuk1975@gmail.com>
2023-04-27 00:45:55 +0530

    Updates

---
+++ precache-manifest.821e9389fac486601ab04034184fa27c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac7902012ab28bd917c",
+    "url": "/currency-converter/static/css/main.9ac23011.chunk.css"
+  },
+  {
+    "revision": "5ac7902012ab28bd917c",
+    "url": "/currency-converter/static/js/main.362f97ef.chunk.js"
+  },
+  {
+    "revision": "aa41bdf78099fd69e2c2",
+    "url": "/currency-converter/static/js/runtime~main.f7adab8a.js"
+  },
+  {
+    "revision": "e61e495545ce71c0d887",
+    "url": "/currency-converter/static/css/2.ee736321.chunk.css"
+  },
+  {
+    "revision": "e61e495545ce71c0d887",
+    "url": "/currency-converter/static/js/2.156bd168.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/currency-converter/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/currency-converter/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c740b580cbc13a1a4d0f20a70f7efa11",
+    "url": "/currency-converter/static/media/currency.c740b580.jpg"
+  },
+  {
+    "revision": "acf4dc3b0b36c462e6c8637d1aea9c68",
+    "url": "/currency-converter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad52c45ff42386685ef55148f4941f63.js"
+  "/currency-converter/precache-manifest.821e9389fac486601ab04034184fa27c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/currency-converter/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
